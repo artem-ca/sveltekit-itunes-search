0c18aae6aa50d3d10af995b9a54af0941305ea98
artem-ca <capuchin@live.ru>
2022-08-23 01:02:00 +0300

    added gif on main page for fun

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,30 +1,17 @@
 <script>
     import { goto } from '$app/navigation'
-    import { onMount } from 'svelte'
-    import { each } from 'svelte/internal'
 
     $: searched = ''
 
-    $: artists = []
-
     const search = () => {
-        goto(`${searched}`)
+        goto(`${searched.trim()}`)
     }
 
     function handleKeydown(event) {
         if (event.keyCode === 13) {
-            goto(`${searched}`)
+            goto(`${searched.trim()}`)
         }
     }
-
-    onMount(async () => {
-        const itunesSearched = await fetch(
-            `https://itunes.apple.com/search?term=${searched}&entity=musicArtist&limit=10`
-        )
-        var res = await itunesSearched.json()
-        artists = res.results
-        console.log(artists)
-    })
 </script>
 
 <svelte:head>
@@ -32,33 +19,33 @@
 </svelte:head>
 
 <section>
-    <div class="flex flex-col items-center justify-center max-w-4xl m-auto">
+    <div class="m-auto flex max-w-4xl flex-col items-center justify-center">
         <a
             href="/"
-            class="text-5xl text-center text-white font-semibold mb-12 opacity-95 hover:opacity-90"
+            class="mb-12 text-center text-5xl font-semibold text-white opacity-95 hover:opacity-90"
         >
             Search an artist
         </a>
         <div
-            class="w-full flex p-1 rounded-lg border 
-                bg-white bg-opacity-20 border-white border-opacity-40"
+            class="flex w-full rounded-lg border border-white 
+                border-opacity-40 bg-white bg-opacity-20 p-1"
         >
             <input
-                class="w-4/5 bg-transparent text-white mx-1 outline-none text-2xl"
+                class="outline-none mx-1 w-4/5 bg-transparent text-2xl text-white"
                 type="text"
                 bind:value={searched}
                 on:keydown={handleKeydown}
             />
             <button
-                class="w-1/5 p-2 rounded-md font-semibold bg-gradient-to-r from-pink-200 via-red-100 to-purple-200 opacity-90 hover:opacity-80"
+                class="w-1/5 rounded-md bg-gradient-to-r from-pink-200 via-red-100 to-purple-200 p-2 font-semibold opacity-90 hover:opacity-80"
                 on:click={search}>Search</button
             >
         </div>
 
-        <div>
-            {#each artists as artist}
-                <p>{artist.artistName}</p>
-            {/each}
-        </div>
+        <img
+            src="static/retro.gif"
+            alt="img"
+            class="shadow-3xl mt-20 max-w-xs rounded-xl"
+        />
     </div>
 </section>
